4a68c982c8d480a231d63e9c177dea486128d214
mBoegvald <miklasbogvald@gmail.com>
2020-04-13 17:58:41 +0200

    nyIndex

---
+++ nyIndex.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="minStil.css" />
+    <link rel="stylesheet" href="footer.css">
+    <link
+      href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+      rel="stylesheet"
+    />
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <section id="intro">
+            <div class="intro-content">
+                <div class="intro-element">
+                    <div class="intro-element-header">
+                        <img src="learning.png" alt="">
+                        <h1>Overskrift</h1>
+                    </div>
+                    <div class="intro-element-text">
+                        <p>
+                            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+                            aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                            voluptatem sequi nesciunt. Neque porro quisquam est. t.
+                        </p>
+                    </div>
+                </div>
+                <div class="intro-element">
+                    <div class="intro-element-header">
+                    <img src="house.png" alt="house"/>
+                    <h1>Overskrift</h1>
+                    </div>
+                    <div class="intro-element-text">
+                        <p>
+                            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+                            aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                            voluptatem sequi nesciunt. Neque porro quisquam est. t.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="banner">
+            <div class="banner-content">
+                <div class="banner-text">
+                    <h2>Overskrift</h2>
+                    <p>
+                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+                        fugit, sed quia consequuntur magni dolores eos qui ratione
+                        voluptatem sequi nesciunt. Neque porro quisquam est. t.
+                    </p>
+                </div>
+                <div class="banner-buttons">
+                    <button class="btn signup-button">Sign up</button>
+                    <button class="btn read-more-button">Read more</button>
+                </div>
+            </div>
+        </section>
+        <section id="courses">
+            <div class="courses-intro"><h1>Overskrift</h1>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut aut inventore exercitationem necessitatibus repellendus mollitia ratione iure debitis, fuga est cupiditate eum, quasi velit praesentium ad corrupti modi aperiam dolore!
+                </p>
+            </div>
+            <div class="courses-container">
+                <div class="course">
+                    <img src="placeholder.jpg" alt="">
+                    <div class="course-text">
+                        <h3>Overskrift</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore
+                        </p>
+                    </div>
+                    <div class="course-link">
+                        <a href="">Start learning</a>
+                    </div>
+                </div>
+                <div class="course">
+                    <img src="placeholder.jpg" alt="">
+                    <div class="course-text">
+                        <h3>Overskrift</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore
+                        </p>
+                    </div>
+                    <div class="course-link">
+                        <a href="">Start learning</a>
+                    </div>
+                </div>
+                <div class="course">
+                    <img src="placeholder.jpg" alt="">
+                    <div class="course-text">
+                        <h3>Overskrift</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore
+                        </p>
+                    </div>
+                    <div class="course-link">
+                        <a href="">Start learning</a>
+                    </div>
+                </div>
+                <div class="course">
+                    <img src="placeholder.jpg" alt="">
+                    <div class="course-text">
+                        <h3>Overskrift</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore
+                        </p>
+                    </div>
+                    <div class="course-link">
+                        <a href="">Start learning</a>
+                    </div>
+                </div>
+                <div class="course">
+                    <img src="placeholder.jpg" alt="">
+                    <div class="course-text">
+                        <h3>Overskrift</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore
+                        </p>
+                    </div>
+                    <div class="course-link">
+                        <a href="">Start learning</a>
+                    </div>
+                </div>
+                <div class="course">
+                    <img src="placeholder.jpg" alt="">
+                    <div class="course-text">
+                        <h3>Overskrift</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore
+                        </p>
+                    </div>
+                    <div class="course-link">
+                        <a href="">Start learning</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
