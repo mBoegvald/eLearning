9ef3b92a77358d91face6bb33552e211a8e39f84
David Fraenkel <davi1360@stud.kea.dk>
2020-04-13 12:40:47 +0200

    Mangler logo

---
+++ footer.php
@@ -0,0 +1,28 @@
+<footer>
+  <div class="footer-container">
+    <div class="footer-box footer-logo">
+      <a href="#"><img src="burger.png" alt="" /></a>
+    </div>
+    <div class="footer-box">
+      <h3>Head office</h3>
+      <a href="#">+45 12345678</a>
+      <a href="#">email@email.dk</a>
+      <a href="#">address 1 3330</a>
+    </div>
+    <div class="footer-box">
+      <h3>Company</h3>
+      <a href="#">Data privacy policy</a>
+    </div>
+    <div class="footer-box">
+      <h3>Join our newsletter!</h3>
+      <p>
+        Subscribe to our marketing newsletter to get the latest tips and advice
+        delivered to your inbox each month!
+      </p>
+      <div>
+        <input type="text" placeholder="Your E-mail" />
+        <button type="submit">Subscribe</button>
+      </div>
+    </div>
+  </div>
+</footer>
