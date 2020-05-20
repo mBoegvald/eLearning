010472ebc2ce008514167a76dda17b4e6b150094
mBoegvald <miklasbogvald@gmail.com>
2020-05-20 17:48:03 +0200

    New design mobile start

--- index.php
+++ index.php
@@ -3,33 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/main.css" />
     <link rel="stylesheet" href="css/nav.css">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/courses.css">
-    <link
+    <!-- <link
       href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
       rel="stylesheet"
     />
     <link
       href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
       rel="stylesheet"
-    />
+    /> -->
     <title>E-learning</title>
 </head>
 <body>
     <?php
-        include_once("components/nav.php");
+        include_once("components/nav.php"); 
     ?>
-    <main>
-        <section id="intro">
-            <div class="intro-content">
-                <div class="intro-element">
-                    <div class="intro-element-header">
+    <main id="landing">
+        <div class="contentContainer">
+            <div class="mainContent">
+                <h1><span class="emphasize">Learn</span> how<br> to build <span class="emphasize">databases</span></h1>
+                <p>Aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est. t. </p>
+                <button class="btn">Get started</button>
+            </div>
+        </div>
+    </main>
+    <section id="information">
+        <div class="contentContainer">
+            <div class="info-content">
+                <div class="info-element">
+                    <div class="info-element-header">
                         <img src="./pics/learning.png" alt="">
                         <h1>Overskrift</h1>
                     </div>
-                    <div class="intro-element-text">
+                    <div class="info-element-text">
                         <p>
                             Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
                             aut fugit, sed quia consequuntur magni dolores eos qui ratione
@@ -37,12 +47,13 @@
                         </p>
                     </div>
                 </div>
-                <div class="intro-element">
-                    <div class="intro-element-header">
-                    <img src="./pics/house.png" alt="house"/>
-                    <h1>Overskrift</h1>
+                <hr>
+                <div class="info-element">
+                    <div class="info-element-header">
+                        <img src="./pics/house.png" alt="house"/>
+                        <h1>Overskrift</h1>
                     </div>
-                    <div class="intro-element-text">
+                    <div class="info-element-text">
                         <p>
                             Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
                             aut fugit, sed quia consequuntur magni dolores eos qui ratione
@@ -51,36 +62,75 @@
                     </div>
                 </div>
             </div>
-        </section>
-        <section id="banner">
-            <div class="banner-content">
-                <div class="banner-text">
-                    <h2>Overskrift</h2>
-                    <p>
-                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
-                        fugit, sed quia consequuntur magni dolores eos qui ratione
-                        voluptatem sequi nesciunt. Neque porro quisquam est. t.
-                    </p>
-                </div>
-                <div class="banner-buttons">
-                    <button class="btn signup-button">Sign up</button>
-                    <button class="btn read-more-button">Read more</button>
-                </div>
+        </div>
+    </section>
+    <section id="reviews">
+        <div class="contentContainer">
+            <h1>Here is what our users say</h1>
+            <div class="mainContent">
+                <h1>Overskrift</h1>
+                <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est. t. </p>
             </div>
-        </section>
-        <section id="courses">
-            <div class="courses-intro"><h1>Overskrift</h1>
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut aut inventore exercitationem necessitatibus repellendus mollitia ratione iure debitis, fuga est cupiditate eum, quasi velit praesentium ad corrupti modi aperiam dolore!
-                </p>
-            </div>
-            <?php 
-                include_once("components/courses.php");
-            ?>
-        </section>
-    </main>
-    <?php
-        include_once("components/footer.html");
-    ?>
+        </div>
+    </section>
 </body>
 </html>
+
+
+
+
+
+
+                    <!-- <section id="intro">
+                        <div class="intro-content">
+                            <div class="intro-element">
+                                <div class="intro-element-header">
+                                    <img src="./pics/learning.png" alt="">
+                                    <h1>Overskrift</h1>
+                                </div>
+                                <div class="intro-element-text">
+                                    <p>
+                                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+                                        aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                                        voluptatem sequi nesciunt. Neque porro quisquam est. t.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="intro-element">
+                                <div class="intro-element-header">
+                                <img src="./pics/house.png" alt="house"/>
+                                <h1>Overskrift</h1>
+                                </div>
+                                <div class="intro-element-text">
+                                    <p>
+                                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+                                        aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                                        voluptatem sequi nesciunt. Neque porro quisquam est. t.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section id="banner">
+                        <div class="banner-content">
+                            <div class="banner-text">
+                                <h2>Overskrift</h2>
+                                <p>
+                                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+                                    fugit, sed quia consequuntur magni dolores eos qui ratione
+                                    voluptatem sequi nesciunt. Neque porro quisquam est. t.
+                                </p>
+                            </div>
+                            <div class="banner-buttons">
+                                <button class="btn signup-button">Sign up</button>
+                                <button class="btn read-more-button">Read more</button>
+                            </div>
+                        </div>
+                    </section>
+                    <section id="courses">
+                        <div class="courses-intro"><h1>Overskrift</h1>
+                            <p>
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut aut inventore exercitationem necessitatibus repellendus mollitia ratione iure debitis, fuga est cupiditate eum, quasi velit praesentium ad corrupti modi aperiam dolore!
+                            </p>
+                        </div>
+                    </section> -->
