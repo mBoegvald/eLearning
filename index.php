d4be1b2fec1ece9b7ccae0e6e4b2187a84be0028
mBoegvald <miklasbogvald@gmail.com>
2020-05-22 11:59:26 +0200

    icons done mobile

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="css/icons.css" />
     <link rel="stylesheet" href="css/nav.css">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/courses.css">
@@ -24,8 +25,14 @@
     ?>
     <main id="landing">
         <div class="contentContainer">
+        <img id="play" class="icons" src="pics/play.png" alt="">
+        <img id="computer" class="icons" src="pics/computer.png" alt="">
+        <img id="brain" class="icons" src="pics/brain.png" alt="">
+        <img id="book" class="icons" src="pics/book.png" alt="">
+        <img id="headphones" class="icons" src="pics/headphones.png" alt="">
+        <img id="mouse" class="icons" src="pics/mouse.png" alt="">
             <div class="mainContent">
-                <h1><span class="emphasize">Learn</span> how<br> to build <span class="emphasize">databases</span></h1>
+                <h1><span class="emphasize">Learn</span> how<br> to build<br> <span class="emphasize">databases</span></h1>
                 <p>Aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est. t. </p>
                 <button class="btn">Get started</button>
             </div>
